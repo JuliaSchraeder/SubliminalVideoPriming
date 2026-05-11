fd34b75f3f217146bc498ed43142a753286cb97a
Julia Schräder <jschraeder@ukaachen.de>
2026-05-11 10:44:36 +0200

    upload paradigm

---
+++ paradigm/Rating.py
@@ -0,0 +1,214 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This experiment was created using PsychoPy3 Experiment Builder (v2020.2.6),
+    on Tue Apr 27 16:48:44 2021
+If you publish work using this script the most relevant publication is:
+
+    Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
+        PsychoPy2: Experiments in behavior made easy Behav Res 51: 195. 
+        https://doi.org/10.3758/s13428-018-01193-y
+
+"""
+
+from __future__ import absolute_import, division
+
+from psychopy import locale_setup
+from psychopy import prefs
+from psychopy import sound, gui, visual, core, data, event, logging, clock, hardware
+from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
+                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
+
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+from numpy.random import random, randint, normal, shuffle
+import os  # handy system and path functions
+import sys  # to get file system encoding
+
+from psychopy.hardware import keyboard
+
+
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Store info about the experiment session
+psychopyVersion = '2020.2.6'
+expName = 'Rating'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001'}
+dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath='/Users/julia/Desktop/Promotion/DFG_Projekt/Paradigmen/VideoMask/Rating.py',
+    savePickle=True, saveWideText=True,
+    dataFileName=filename)
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.EXP)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+frameTolerance = 0.001  # how close to onset before 'same' frame
+
+# Start Code - component code to be run after the window creation
+
+# Setup the Window
+win = visual.Window(
+    size=(1024, 768), fullscr=True, screen=0, 
+    winType='pyglet', allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+    blendMode='avg', useFBO=True, 
+    units='height')
+# store frame rate of monitor if we can measure it
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] != None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
+else:
+    frameDur = 1.0 / 60.0  # could not measure, so guess
+
+# create a default keyboard (e.g. to check for escape)
+defaultKeyboard = keyboard.Keyboard()
+
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+slider = visual.Slider(win=win, name='slider',
+    size=(1.0, 0.1), pos=(0, -0.4), units=None,
+    labels=None, ticks=(1, 2, 3, 4, 5, 6, 7),
+    granularity=0, style=('rating', 'whiteOnBlack'),
+    color='LightGray', font='HelveticaBold',
+    flip=False, depth=0)
+text_slider = visual.TextStim(win=win, name='text_slider',
+    text='traurig                       glücklich',
+    font='Arial',
+    pos=(0, -0.2), height=0.07, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1, 
+    languageStyle='LTR',
+    depth=-2.0);
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+
+# ------Prepare to start Routine "trial"-------
+continueRoutine = True
+routineTimer.add(1.000000)
+# update component parameters for each repeat
+slider.reset()
+event.clearEvents('keyboard')
+slider.markerPos = 3
+# keep track of which components have finished
+trialComponents = [slider, text_slider]
+for thisComponent in trialComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+trialClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+
+# -------Run Routine "trial"-------
+while continueRoutine and routineTimer.getTime() > 0:
+    # get current time
+    t = trialClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=trialClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *slider* updates
+    if slider.status == NOT_STARTED and frameN >= 0:
+        # keep track of start time/frame for later
+        slider.frameNStart = frameN  # exact frame index
+        slider.tStart = t  # local t and not account for scr refresh
+        slider.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(slider, 'tStartRefresh')  # time at next scr refresh
+        slider.setAutoDraw(True)
+    if slider.status == STARTED:
+        if frameN >= 300:
+            # keep track of stop time/frame for later
+            slider.tStop = t  # not accounting for scr refresh
+            slider.frameNStop = frameN  # exact frame index
+            win.timeOnFlip(slider, 'tStopRefresh')  # time at next scr refresh
+            slider.setAutoDraw(False)
+    keys = event.getKeys()
+    
+    if len(keys):
+        if '7' in keys:
+            slider.markerPos = slider.markerPos - 1
+        elif '9' in keys:
+            slider.markerPos = slider.markerPos  + 1 
+    
+    # *text_slider* updates
+    if text_slider.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        text_slider.frameNStart = frameN  # exact frame index
+        text_slider.tStart = t  # local t and not account for scr refresh
+        text_slider.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(text_slider, 'tStartRefresh')  # time at next scr refresh
+        text_slider.setAutoDraw(True)
+    if text_slider.status == STARTED:
+        # is it time to stop? (based on global clock, using actual start)
+        if tThisFlipGlobal > text_slider.tStartRefresh + 1.0-frameTolerance:
+            # keep track of stop time/frame for later
+            text_slider.tStop = t  # not accounting for scr refresh
+            text_slider.frameNStop = frameN  # exact frame index
+            win.timeOnFlip(text_slider, 'tStopRefresh')  # time at next scr refresh
+            text_slider.setAutoDraw(False)
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "trial"-------
+for thisComponent in trialComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('slider.response', slider.getRating())
+thisExp.addData('slider.rt', slider.getRT())
+thisExp.addData('slider.started', slider.tStartRefresh)
+thisExp.addData('slider.stopped', slider.tStopRefresh)
+thisExp.addData("Rating", slider.markerPos)
+thisExp.addData('text_slider.started', text_slider.tStartRefresh)
+thisExp.addData('text_slider.stopped', text_slider.tStopRefresh)
+
+# Flip one final time so any remaining win.callOnFlip() 
+# and win.timeOnFlip() tasks get executed before quitting
+win.flip()
+
+# these shouldn't be strictly necessary (should auto-save)
+thisExp.saveAsWideText(filename+'.csv', delim='auto')
+thisExp.saveAsPickle(filename)
+logging.flush()
+# make sure everything is closed down
+thisExp.abort()  # or data files will save again on exit
+win.close()
+core.quit()
